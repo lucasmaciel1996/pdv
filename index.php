71322a514e36591f7dff802ed172a3836a8afa27
aluno <lucas333maciel@gmail.com>
2018-09-26 20:37:11 -0300

    Fist Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+        
+    </head>
+    <body>
+        
+        <!--inicio Container -->
+        <div class="container">
+          <!--Row -->
+            <div class="row">
+                <div class="col-sm">
+                  Couna 1
+                </div>
+                <div class="col-sm">
+                 Coluna 2
+                </div>
+              </div>
+             <!--Fim Row -->
+        </div>
+        <!--Fim Container -->  
+    </body>
+</html>
